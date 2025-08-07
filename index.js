a23cc3145e98db4ca014a92ac4f7ab1c4998e2b0
Barbara Goldbeck <goldbeck.barbara@gmail.com>
2025-08-07 19:48:56 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
 
-  get(pos) {}
+    this.items.sort((function (a, b) {
+      return a - b;
+    }));
+    this.length = this.items.length; 
+  }
 
-  max() {}
+  get(pos) {
+    if (pos < 0 || pos >= this.items.length) {
+      throw new Error("OutOfBounds"); 
+    } else {
+    return this.items[pos]
+   }; 
+  }
 
-  min() {}
+  max() { 
+    if (this.items.length === 0) {
+      throw new Error ("EmptySortedList"); 
+    } else {
 
-  sum() {}
+      return Math.max(...this.items); 
+    }
+  }
 
-  avg() {}
+  min() { 
+    if (this.items.length === 0) {
+      throw new Error ("Empty SortedList")
+    } else {
+      return Math.min(...this.items); 
+    }
+  }
+
+  sum() {
+    if (this.items.length === 0) {
+      return 0; 
+    } else {
+      return this.items.reduce((acc, val) => acc + val); 
+    }
+   }
+
+  avg() { 
+    let result = 0; 
+    if (this.items.length === 0) {
+      throw new Error ('EmptySortedList'); 
+    } else {
+      result = this.items.reduce((acc, val) => acc + val); 
+    }
+    return result / this.items.length; 
+  }
 }
 
 module.exports = SortedList;
